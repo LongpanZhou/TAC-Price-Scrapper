4c82437fde2f5e321a3497490b1b31cb2f8afce4
LongpanZhou <zhoul83@mcmaster.ca>
2024-03-31 16:13:49 -0400

    fix self.data from list to set

--- backend/Scrapper.py
+++ backend/Scrapper.py
@@ -6,14 +6,14 @@ from typing import *
 class Scrapper:
     def __init__(self):
         self.scrappers = [AmazonScrapper(),GoogleScrapper(),EbayScrapper()]
-        self.data = []
+        self.data = set()
 
     @lru_cache(maxsize=None)
     def __call__(self, name: str) -> List[tuple[str, str, str, str]]:
-        self.data = []
+        self.data.clear()
         with alive_bar(3, title='Processing') as bar:
             for obj in self.scrappers:
                 for _ in obj(name):
-                    self.data.append(_)
+                    self.data.add(_)
                 bar()
-            return self.data
+            return list(self.data)
